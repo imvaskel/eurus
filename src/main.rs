d131df2d62678bd4a7ba7b6048585ffb8acb36b4
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2024-09-07 17:32:15 -0400

    add traefik script

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 #![allow(dead_code)]
 
-use std::{fmt::Display, sync::LazyLock};
+use std::{
+    collections::HashMap,
+    fmt::Display,
+    path::{Path, PathBuf},
+    sync::LazyLock,
+};
 
-use color_eyre::eyre::{bail, Context, ContextCompat, Result};
+use color_eyre::eyre::{bail, ensure, Context, ContextCompat, Result};
 use directories::ProjectDirs;
-use inquire::{autocompletion::Replacement, Autocomplete, Select, Text};
+use docker_compose_types::{Compose, ComposeNetwork, Labels, MapOrEmpty, NetworkSettings, Networks};
+use inquire::{Autocomplete, Editor, Select, Text};
 use reqwest::blocking::Client;
 use serde::{Deserialize, Serialize};
 
 static BASE_URL: &str = "https://api.cloudflare.com/client/v4";
-static CLIENT: LazyLock<Client> = LazyLock::new(|| Client::new());
+static CLIENT: LazyLock<Client> = LazyLock::new(Client::new);
+static PROJECT_DIR: LazyLock<ProjectDirs> =
+    LazyLock::new(|| ProjectDirs::from("gay", "vaskel", "eurus").unwrap());
+static CONFIG_DIR: LazyLock<&Path> = LazyLock::new(|| PROJECT_DIR.config_dir());
 
 #[derive(Debug, Deserialize, Default, Clone)]
 struct CloudflareResponse<T> {
@@ -46,7 +55,7 @@ struct DnsCreateUpdate {
     #[serde(rename = "type")]
     record_type: String,
     proxied: bool,
-    content: String
+    content: String,
 }
 
 #[derive(Debug, Deserialize, Serialize, Default)]
@@ -63,8 +72,9 @@ impl Display for ZoneInfo {
 
 #[derive(Debug, Deserialize, Serialize, Default)]
 struct Config {
-    email: String,
     zones: Vec<ZoneInfo>,
+    traefik_network: String,
+    traefik_tls: String,
 }
 
 #[derive(Debug, Clone)]
@@ -82,7 +92,7 @@ impl Autocomplete for SubdomainAutoComplete {
             self.current
                 .iter()
                 .filter(|i| i.contains(&input))
-                .map(|s| String::from(s))
+                .map(String::from)
                 .collect(),
         )
     }
@@ -92,16 +102,51 @@ impl Autocomplete for SubdomainAutoComplete {
         _: &str,
         highlighted_suggestion: Option<String>,
     ) -> std::result::Result<inquire::autocompletion::Replacement, inquire::CustomUserError> {
-        std::result::Result::Ok(match highlighted_suggestion {
-            Some(s) => Replacement::Some(s),
-            None => Replacement::None,
-        })
+        std::result::Result::Ok(highlighted_suggestion)
     }
 }
 
-fn main() -> Result<()> {
-    color_eyre::install()?;
+fn get_config() -> Result<Config> {
+    std::fs::DirBuilder::new()
+        .recursive(true)
+        .create(*CONFIG_DIR)
+        .context("Failed to create config directory")?;
 
+    serde_json::from_str(&std::fs::read_to_string(&*CONFIG_DIR.join("config.json"))?)
+            .context("Configuration is malformed.")
+}
+
+fn prompt_new_zone_config(api_key: &str) -> Result<Config> {
+    let zone_id = Text::new("Zone ID:")
+        .with_help_message("This can be found on the dashboard.")
+        .prompt()?;
+
+    let res: CloudflareResponse<ZoneDetailsResponse> = (*CLIENT)
+        .get(format!("{}/zones/{}", BASE_URL, zone_id))
+        .bearer_auth(api_key)
+        .send()?
+        .json::<CloudflareResponse<ZoneDetailsResponse>>()?;
+
+    if !res.errors.is_empty() {
+        bail!("Cloudflare api returned an error: {:?}", res.errors);
+    }
+
+    let conf = Config {
+        zones: vec![ZoneInfo {
+            id: zone_id,
+            name: res.result.unwrap().name, // We check for errors earlier.
+        }],
+        ..Default::default()
+    };
+    std::fs::write(
+        (*CONFIG_DIR).join("config.json"),
+        serde_json::to_string(&conf)?,
+    )?;
+
+    Ok(conf)
+}
+
+fn dns() -> Result<()> {
     let api_key = std::env::var("CF_API_KEY").or_else(|_| {
         Text::new("Enter your api key.")
             .with_help_message(
@@ -110,46 +155,15 @@ fn main() -> Result<()> {
             .prompt()
     })?;
 
-    let proj_dirs = ProjectDirs::from("gay", "vaskel", "eurus")
-        .context("Failed to get configuration directory.")?;
-
-    let config_dir = proj_dirs.config_dir();
-    std::fs::DirBuilder::new()
-        .recursive(true)
-        .create(&config_dir)
-        .context("Failed to create config directory")?;
-
-    let config: Config = match std::fs::read_to_string(config_dir.join("config.json")) {
-        Ok(s) => serde_json::from_str(&s).context("Configuration is malformed.")?,
-        Err(_) => {
-            let zone_id = Text::new("Zone ID:")
-                .with_help_message("This can be found on the dashboard.")
-                .prompt()?;
-
-            let res: CloudflareResponse<ZoneDetailsResponse> = (*CLIENT)
-                .get(format!("{}/zones/{}", BASE_URL, zone_id))
-                .bearer_auth(&api_key)
-                .send()?
-                .json::<CloudflareResponse<ZoneDetailsResponse>>()?;
-
-            if res.errors.len() != 0 {
-                bail!("Cloudflare api returned an error: {:?}", res.errors);
+    let config: Config = match get_config() {
+        Ok(c) => {
+            if c.zones.is_empty() {
+                prompt_new_zone_config(&api_key)?
+            } else {
+                c
             }
-
-            let conf = Config {
-                email: "".into(),
-                zones: vec![ZoneInfo {
-                    id: zone_id,
-                    name: res.result.unwrap().name, // We check for errors earlier.
-                }],
-            };
-            std::fs::write(
-                config_dir.join("config.json"),
-                serde_json::to_string(&conf)?,
-            )?;
-
-            conf
         }
+        Err(_) => prompt_new_zone_config(&api_key)?,
     };
 
     let domain = Select::new("Select a zone", config.zones).prompt()?;
@@ -160,7 +174,7 @@ fn main() -> Result<()> {
         .send()?
         .json()?;
 
-    if res.errors.len() != 0 {
+    if !res.errors.is_empty() {
         bail!("Cloudflare api returned an error: {:?}", res.errors);
     }
 
@@ -181,15 +195,14 @@ fn main() -> Result<()> {
 
     let info = domains
         .iter()
-        .find(|d| d.name == subdomain.clone())
-        .map(|f| f.clone());
+        .find(|d| d.name == subdomain.clone()).cloned();
 
     let body = DnsCreateUpdate {
         name: subdomain,
         id: None,
         proxied: true,
         record_type,
-        content: target
+        content: target,
     };
 
     let res: CloudflareResponse<DnsListResponse> = if info.is_some() {
@@ -212,11 +225,210 @@ fn main() -> Result<()> {
             .json()?
     };
 
-    if res.errors.len() != 0 {
+    if !res.errors.is_empty() {
         bail!("Cloudflare api returned an error: {:?}", res.errors);
     }
 
     println!("Done!");
 
     Ok(())
+}
+
+fn add_or_ignore_label(labels: &mut Labels, key: &str, value: &str) {
+    match labels {
+        Labels::List(l) => {
+            let label = format!("{}={}", key, value);
+            if !l.contains(&label) {
+                l.push(label);
+            }
+        }
+        Labels::Map(m) => {
+            if !m.contains_key(key) {
+                m.insert(key.to_string(), value.to_string());
+            }
+        }
+    }
+}
+
+fn traefik() -> Result<()> {
+    static STATIC_LABELS: LazyLock<HashMap<&str, &str>> = LazyLock::new(|| {
+        HashMap::from([
+            ("traefik.enable", "true"),
+            (
+                "traefik.http.routers.${COMPOSE_PROJECT_NAME}.entrypoints",
+                "websecure",
+            ),
+            ("traefik.http.routers.${COMPOSE_PROJECT_NAME}.tls", "true"),
+        ])
+    });
+    static RULE_LABEL: &str = "traefik.http.routers.${COMPOSE_PROJECT_NAME}.rule";
+    static CERT_RESOLVER_LABEL: &str =
+        "traefik.http.routers.${COMPOSE_PROJECT_NAME}.tls.certresolver";
+    static TRAEFIK_NETWORK_LABEL: &str = "traefik.docker.network";
+    static TRAEFIK_SERVICE_LABEL: &str = "";
+    static TRAEFIK_SERVICE_PORT_LABEL: &str =
+        "traefik.http.services.${COMPOSE_PROJECT_NAME}.loadbalancer.server.port";
+
+    let config = match get_config() {
+        Ok(mut c) => {
+            if c.traefik_network.is_empty() {
+                let network = Text::new("Enter the network that traefik is on.").prompt()?;
+                c.traefik_network = network;
+            }
+            if c.traefik_tls.is_empty() {
+                let tls = Text::new("Enter your TLS provider for traefik.").prompt()?;
+                c.traefik_tls = tls;
+            }
+            std::fs::write(
+                (*CONFIG_DIR).join("config.json"),
+                serde_json::to_string(&c)?,
+            )?;
+            c
+        }
+        Err(_) => {
+            let network = Text::new("Enter the network that traefik is on.").prompt()?;
+            let tls = Text::new("Enter your TLS provider for traefik.").prompt()?;
+            let config = Config {
+                traefik_network: network,
+                traefik_tls: tls,
+                ..Default::default()
+            };
+            std::fs::write(
+                (*CONFIG_DIR).join("config.json"),
+                serde_json::to_string(&config)?,
+            )?;
+            config
+        }
+    };
+
+    let file = match std::env::args().nth(2) {
+        Some(s) => PathBuf::from(s),
+        None => {
+            static COMPOSE_PATHS: [&str; 2] = ["compose.yaml", "docker-compose.yaml"];
+
+            COMPOSE_PATHS
+                .iter()
+                .find(|p| Path::new(p).exists())
+                .map(PathBuf::from)
+                .context("Could not find valid docker-compose file.")?
+        }
+    };
+
+    ensure!(Path::new(&file).exists(), "The file provided should exist.");
+
+    let contents = std::fs::read_to_string(&file).context("Could not read the file contents.")?;
+    let mut compose: Compose =
+        serde_yml::from_str(&contents).context("The compose yaml was invalid.")?;
+
+    let services = compose.services.0.iter().filter(|e| e.1.is_some());
+    let selected_service = Select::new(
+        "Select the service to add traefik to",
+        services.map(|s| s.0).collect(),
+    )
+    .prompt()?;
+
+    let service = compose
+        .services
+        .0
+        .get(selected_service)
+        .context("Could not get the service.")?;
+
+    let rule = Editor::new("Enter the rule for this service.").prompt()?;
+    let port: Option<u16> = loop {
+        let text = Text::new("Enter the port this application exposes")
+            .with_default("None")
+            .prompt_skippable()?;
+
+        match text.as_deref() {
+            Some("None") => break None,
+            Some(t) => match t.parse() {
+                Ok(n) => break Some(n),
+                Err(_) => continue,
+            },
+            None => break None,
+        }
+    };
+
+    ensure!(service.is_some(), "The service is none.");
+
+    let mut service = service.clone().unwrap();
+    for (key, value) in &*STATIC_LABELS {
+        add_or_ignore_label(&mut service.labels, key, value);
+    }
+
+    add_or_ignore_label(&mut service.labels, RULE_LABEL, &rule);
+    add_or_ignore_label(
+        &mut service.labels,
+        CERT_RESOLVER_LABEL,
+        &config.traefik_tls,
+    );
+    add_or_ignore_label(
+        &mut service.labels,
+        TRAEFIK_NETWORK_LABEL,
+        &config.traefik_network,
+    );
+
+    if port.is_some() {
+        let port = port.unwrap();
+        add_or_ignore_label(
+            &mut service.labels,
+            TRAEFIK_SERVICE_PORT_LABEL,
+            &port.to_string(),
+        );
+    }
+
+    // get or make the network settings for the traefik network
+    let mut network = compose
+        .networks
+        .0
+        .get(&config.traefik_network).map(|n| match n {
+                MapOrEmpty::Empty => NetworkSettings {
+                    ..Default::default()
+                },
+                MapOrEmpty::Map(m) => m.clone(),
+            })
+        .unwrap_or(NetworkSettings {
+            ..Default::default()
+        }); // Should never be None
+
+    network.external = Some(ComposeNetwork::Bool(true));
+
+    compose.networks.0.insert(config.traefik_network.clone(), MapOrEmpty::Map(network));
+
+    match &mut service.networks {
+        Networks::Simple(a) => {
+            if !a.contains(&config.traefik_network) {
+                a.push(config.traefik_network);
+            }
+        }
+        Networks::Advanced(a) => {
+            a.0.insert(config.traefik_network, MapOrEmpty::Empty);
+        }
+    }
+
+    compose
+        .services
+        .0
+        .insert(selected_service.clone(), Some(service));
+
+    std::fs::write(&file, serde_yml::to_string(&compose)?)?;
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    let cmd = match std::env::args().nth(1).map(|f| f.to_lowercase()) {
+        Some(s) => s,
+        None => {
+            bail!("no command given.");
+        }
+    };
+
+    match cmd.as_str() {
+        "dns" => dns(),
+        "traefik" => traefik(),
+        _ => bail!("Invalid command, it should be ``dns`` or ``traefik``."),
+    }
 }
